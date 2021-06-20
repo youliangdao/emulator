58e34d3daa1eddb0cf0ed999e6468507a6b8be9c
youliangdao <youliangdaojing5@gmail.com>
2021-06-20 13:31:57 +0900

    リアルモードの簡易エミュレータ作成

--- chap02/section3/main.c
+++ chap02/section3/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 
-//メモリは1MB
+//メモリは1MB（つまり、20バイトのメモリ空間を想定）
 #define MEMORY_SIZE (1024 * 1024)
 
 enum Register {EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI, REGISTERS_COUNT};
@@ -51,6 +51,66 @@ void destroy_emu(Emulator* emu){
   free(emu);
 }
 
+//memory配列の指定した位置から8ビットの値を取得する
+uint32_t get_code8(Emulator* emu, int index){
+  return emu->memory[emu->eip + index];
+}
+
+//memory配列の指定した位置から符号付きで8ビットの値を取得する
+int32_t get_sign_code8(Emulator* emu, int index){
+  return (int8_t)emu->memory[emu->eip + index];
+}
+
+//memory配列の指定した位置から32ビットの値を取得する
+uint32_t get_code32(Emulator* emu, int index){
+  uint32_t ret;
+  for (int i = 0; i < 4; i++)
+  {
+    ret |= get_code8(emu, index + i) << (i * 8);
+  }
+  return ret;
+}
+
+
+//汎用レジスタに32ビットのリテラルをコピーするmov命令のエミュレート
+void mov_r32_imm32(Emulator* emu){
+  uint8_t reg = get_code8(emu, 0) - 0xB8;
+  uint32_t value = get_code32(emu, 1);
+  emu->registers[reg] = value;
+  emu->eip += 5;
+}
+
+//1バイトのメモリ番地を取るjmp命令のエミュレート
+void short_jump(Emulator* emu){
+  int8_t diff =  get_sign_code8(emu, 1);
+  emu->eip += (diff + 2);
+}
+
+//関数ポインタテーブルの作成
+typedef void instruction_func_t(Emulator*);
+
+  //関数ポインタ型の配列instructionsの作成と初期化
+instruction_func_t* instructions[256];
+void init_instrutions(void){
+  int i;
+  memset(instructions, 0, sizeof(instructions));
+  for (int i = 0; i < 8; i++)
+  {
+    instructions[0xB8 + i] = mov_r32_imm32;
+  }
+  instructions[0xEB] = short_jump;
+}
+
+//汎用レジスタとプログラムカウンタの値を出力
+void dump_registers(Emulator* emu){
+  for (int i = 0; i < REGISTERS_COUNT; i++)
+  {
+    printf("%s = %08x\n", register_name[i], emu->registers[i]);
+  }
+
+  printf("EIP = %08x\n", emu->eip);
+}
+
 //メイン処理
 int main(int argc, char const *argv[])
 {
@@ -77,6 +137,35 @@ int main(int argc, char const *argv[])
   fread(emu->memory, 1, 0x200, fp);
   fclose(fp);
 
+  //関数ポインタテーブルの作成
+  init_instrutions();
+
+  //命令の実行と終了
+  while (emu->eip < MEMORY_SIZE)
+  {
+    uint8_t code = get_code8(emu, 0);
+
+    //現在のプログラムカウントと実行されるバイナリを出力する
+    printf("EIP = 0x%08X, Code = %02X\n", emu->eip, code);
+
+    if (instructions[code] == NULL)
+    {
+      printf("\n\nNot Implemented: %x\n", code);
+      break;
+    }
+
+    //命令の実行
+    instructions[code](emu);
+
+    //
+    if (emu->eip == 0)
+    {
+      printf("\n\nEnd of program. \n\n");
+      break;
+    }
+  }
+
+  dump_registers(emu);
   destroy_emu(emu);
   return 0;
 }
