678ccf6d3c93c45db682e4a8dd62fd90d1fa1145
youliangdao <youliangdaojing5@gmail.com>
2021-06-28 12:06:02 +0900

    fix

--- chap03/section4/emulator_function.h
+++ chap03/section4/emulator_function.h
@@ -10,6 +10,8 @@ int32_t get_sign_code8(Emulator* emu, int index);
 uint32_t get_code32(Emulator* emu, int index);
 int32_t get_sign_code32(Emulator* emu, int index);
 void set_register32(Emulator* emu, int index, uint32_t value);
+void set_memory8(Emulator* emu, uint32_t value, uint32_t address);
+void set_memory32(Emulator* emu, uint32_t value, uint32_t address);
 
 // 汎用レジスタの指定レジスタの値を取得
 uint32_t get_register32(Emulator* emu, uint8_t index);
