bbbf091d393e3dfea98d062165e78f8507f9ce5b
youliangdao <youliangdaojing5@gmail.com>
2021-06-28 11:10:02 +0900

    mov_rm32_r32とmov_rm32_imm32の実装

--- chap03/section4/emulator_function.c
+++ chap03/section4/emulator_function.c
@@ -27,3 +27,18 @@ int32_t get_sign_code32(Emulator* emu, int index)
 {
   return (int32_t)get_code32(emu, index);
 }
+
+void set_register32(Emulator* emu, int index, uint32_t value)
+{
+  emu->registers[index] = value;
+}
+
+void set_memory32(Emulator* emu, uint32_t value, uint32_t address)
+{
+  uint8_t ret = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    ret |= value >> (8 * i);
+    emu->memory[address + i] = ret;
+  }
+}
---
+++ chap03/section4/modrm.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "emulator.h"
+
+typedef struct
+{
+  uint8_t mod;
+  union
+  {
+    uint8_t reg_index;
+    uint8_t opecode;
+  };
+
+  uint8_t rm;
+  uint8_t sib;
+  union
+  {
+    int8_t disp8;
+    int32_t disp32;
+  };
+
+} ModRM;
+
+void parse_modrm(Emulator* emu, ModRM* modrm);
+void set_rm32(Emulator* emu, ModRM* modrm, uint32_t value);
+uint32_t calc_memory_address(Emulator* emu, ModRM* modrm);
