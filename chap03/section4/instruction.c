95b6835f2d86b46af0de1371ae0e5cea82b3c931
youliangdao <youliangdaojing5@gmail.com>
2021-06-28 13:01:15 +0900

    code_83の実装

--- chap03/section4/instruction.c
+++ chap03/section4/instruction.c
@@ -21,6 +21,32 @@ void add_rm32_r32(Emulator* emu)
   set_rm32(emu, &modrm, rm32 + r32);
 }
 
+void sub_rm32_imm8(Emulator* emu, ModRM* modrm)
+{
+  uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
+  uint32_t rm32 = get_rm32(emu, modrm);
+  set_rm32(emu, modrm, rm32 - imm8);
+}
+
+
+void code_83(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+
+  switch (modrm.opecode)
+  {
+  case 5:
+    sub_rm32_imm8(emu, &modrm);
+    break;
+
+  default:
+    printf("オペコード83の命令は実装されていません\n");
+    exit(1);
+  }
+}
+
 void mov_rm32_r32(Emulator* emu)
 {
   emu->eip += 1;
@@ -78,6 +104,7 @@ void init_instructions(void)
   memset(instructions, 0, sizeof(instructions));
 
   instructions[0x01] = add_rm32_r32;
+  instructions[0x83] = code_83;
   instructions[0x89] = mov_rm32_r32;
   instructions[0x8B] = mov_r32_rm32;
   for (int i = 0; i < 8; i++)
