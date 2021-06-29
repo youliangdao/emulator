a3e0e9fb842318956bb3b5b6146877b4249631e1
youliangdao <youliangdaojing5@gmail.com>
2021-06-29 15:15:54 +0900

    条件分岐命令実装

--- chap03/section10/emulator_function.c
+++ chap03/section10/emulator_function.c
@@ -1,155 +1,145 @@
 #include "emulator_function.h"
 
-//memory配列の指定した位置から8ビットの値を取得する
-uint32_t get_code8(Emulator* emu, int index){
+// 指定されたプログラムカウンタから値を取得する
+uint32_t get_code8(Emulator* emu, int index)
+{
   return emu->memory[emu->eip + index];
 }
 
-//memory配列の指定した位置から符号付きで8ビットの値を取得する
-int32_t get_sign_code8(Emulator* emu, int index){
+// 指定されたプログラムカウンタから値を符号付きで取得する
+int32_t get_sign_code8(Emulator* emu, int index)
+{
   return (int8_t)emu->memory[emu->eip + index];
 }
 
-//memory配列の指定した位置から32ビットの値を取得する
-uint32_t get_code32(Emulator* emu, int index){
-  uint32_t ret;
+// 指定されたプログラムカウンタから4byte領域の値を取得する
+uint32_t get_code32(Emulator* emu, int index)
+{
+  uint32_t ret = 0;
   for (int i = 0; i < 4; i++)
   {
-    ret |= get_code8(emu, index + i) << (i * 8);
+    ret |= get_code8(emu, index + i) << (8 * i);
   }
+
   return ret;
 }
 
-//memory配列の指定した位置から32ビットの値を取得する
-int32_t get_sign_code32(Emulator* emu, int index){
+// 指定されたプログラムカウンタから4byte領域の値を符号付きで取得する
+int32_t get_sign_code32(Emulator* emu, int index)
+{
   return (int32_t)get_code32(emu, index);
 }
 
-//index番目の汎用レジスタに32bitの値を書き込む
-void set_register32(Emulator* emu, int index, uint32_t value){
+//index番目の汎用レジスタに指定された32bit値を代入する
+void set_register32(Emulator* emu, int index, uint32_t value)
+{
   emu->registers[index] = value;
 }
 
-//メモリのaddress番地に8bitの値を書き込む
-void set_memory8(Emulator* emu, uint32_t address, uint32_t value){
+//指定アドレスに指定された32bit値の下位8bit値を格納する
+void set_memory8(Emulator* emu, uint32_t value, uint32_t address)
+{
   emu->memory[address] = value & 0xFF;
 }
 
-//メモリのaddress番地に32bitの値を書き込む
-void set_memory32(Emulator* emu, uint32_t address, uint32_t value){
+//指定アドレスから４byte領域に指定された32bit値を格納する
+void set_memory32(Emulator* emu, uint32_t value, uint32_t address)
+{
   for (int i = 0; i < 4; i++)
   {
-    set_memory8(emu, address + i, value >> (8 * i));
+    set_memory8(emu, value >> (8 * i), address + i);
   }
-
 }
 
-//index番目の32bit汎用レジスタの値を取得する
-uint32_t get_register32(Emulator* emu, int index){
+//index番目の汎用レジスタに格納されている32bit値を取得する
+uint32_t get_register32(Emulator* emu, uint8_t index)
+{
   return emu->registers[index];
 }
 
-//メモリのindex番地の8bitの値を取得する
+//指定メモリアドレスから1byte値を取得する
 uint8_t get_memory8(Emulator* emu, uint32_t address){
   return emu->memory[address];
 }
 
-//メモリのindex番地の32bitの値を取得する
+//指定メモリアドレスから4byte値を取得する
 uint32_t get_memory32(Emulator* emu, uint32_t address){
-  uint32_t ret;
+  uint32_t ret = 0;
   for (int i = 0; i < 4; i++)
   {
     ret |= get_memory8(emu, address + i) << (8 * i);
   }
+
   return ret;
 }
 
-void push32(Emulator* emu, uint32_t value){
-  uint32_t address = get_register32(emu, ESP) -4;
-  set_register32(emu, ESP, address);
-  set_memory32(emu, address, value);
-}
-
-uint32_t pop32(Emulator* emu){
-  uint32_t address = get_register32(emu, ESP);
-  uint32_t ret = get_memory32(emu, address);
-  set_register32(emu, ESP, address + 4);
-  return ret;
-}
-
-void set_carry(Emulator* emu, int is_carry){
+void set_carry(Emulator* emu, int is_carry)
+{
   if (is_carry)
   {
-    emu->eflgas |= CARRY_FLAG;
-  }
-  else
-  {
-    emu->eflgas &= ~CARRY_FLAG;
-  }
-
-}
-
-void set_zero(Emulator* emu, int is_zero){
-  if (is_zero)
-  {
-    emu->eflgas |= ZERO_FLAG;
+    emu->eflags |= CARRY_FLAG;
   } else
   {
-    emu->eflgas &= ~ZERO_FLAG;
+    emu->eflags &= ~CARRY_FLAG;
   }
-
 }
 
-void set_sign(Emulator* emu, int is_sign){
+void set_zero(Emulator* emu, int is_zero)
+{
+  if (is_zero)
+  {
+    emu->eflags |= ZERO_FLAG;
+  } else
+  {
+    emu->eflags &= ~ZERO_FLAG;
+  }
+}
+
+void set_sign(Emulator* emu, int is_sign)
+{
   if (is_sign)
   {
-    emu->eflgas |= SIGN_FLAG;
-  }
-  else
+    emu->eflags |= SIGN_FLAG;
+  } else
   {
-    emu->eflgas &= ~SIGN_FLAG;
+    emu->eflags &= ~SIGN_FLAG;
   }
-
 }
 
-void set_overflow(Emulator* emu, int is_overflow){
+void set_overflow(Emulator* emu, int is_overflow)
+{
   if (is_overflow)
   {
-    emu->eflgas |= OVERFLOW_FLAG;
-  }
-  else
+    emu->eflags |= OVERFLOW_FLAG;
+  } else
   {
-    emu->eflgas &= ~OVERFLOW_FLAG;
+    emu->eflags &= ~OVERFLOW_FLAG;
   }
-
 }
 
 int is_carry(Emulator* emu){
-  return (emu->eflgas & CARRY_FLAG) != 0;
+  return (emu->eflags & CARRY_FLAG) != 0;
 }
 
 int is_zero(Emulator* emu){
-
+  return (emu->eflags & ZERO_FLAG) != 0;
 }
 
 int is_sign(Emulator* emu){
-  return (emu->eflgas & SIGN_FLAG) != 0;
+  return (emu->eflags & SIGN_FLAG) != 0;
 }
 
 int is_overflow(Emulator* emu){
-
+  return (emu->eflags & OVERFLOW_FLAG) != 0;
 }
 
-void update_eflags_sub(
-  Emulator* emu, uint32_t v1, uint32_t v2, uint64_t result
-)
+void update_eflags_sub(Emulator* emu, uint32_t value1, uint32_t value2, uint64_t result)
 {
-  int sign1 = v1 >> 31;
-  int sign2 = v2 >> 31;
+  int sign1 = value1 >> 31;
+  int sign2 = value2 >> 31;
   int signr = (result >> 31) & 1;
-
   set_carry(emu, result >> 32);
-  set_zero(emu, result == 0);
   set_sign(emu, signr);
-  set_overflow(emu, sign1 != sign2 && sign1 != signr);
+  set_zero(emu, result == 0);
+  set_overflow(emu, (sign1 != sign2) && (sign1 != signr));
 }
--- chap03/section10/instruction.c
+++ chap03/section10/instruction.c
@@ -1,206 +1,217 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
-#include "emulator.h"
+#include <string.h>
+
 #include "instruction.h"
+#include "emulator.h"
 #include "emulator_function.h"
+
 #include "modrm.h"
 
-//汎用レジスタに32ビットのリテラルをコピーするmov命令のエミュレート
-void mov_r32_imm32(Emulator* emu){
-  uint8_t reg = get_code8(emu, 0) - 0xB8;
-  uint32_t value = get_code32(emu, 1);
-  emu->registers[reg] = value;
-  emu->eip += 5;
+/* x86命令の配列、opecode番目の関数がx86の
+   opcodeに対応した命令となっている */
+instruction_func_t* instructions[256];
+
+static void mov_r32_imm32(Emulator* emu)
+{
+    uint8_t reg = get_code8(emu, 0) - 0xB8;
+    uint32_t value = get_code32(emu, 1);
+    set_register32(emu, reg, value);
+    emu->eip += 5;
 }
 
-//1バイトのメモリ番地を取るjmp命令のエミュレート
-void short_jump(Emulator* emu){
-  int8_t diff =  get_sign_code8(emu, 1);
-  emu->eip += (diff + 2);
+static void mov_r32_rm32(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t rm32 = get_rm32(emu, &modrm);
+    set_r32(emu, &modrm, rm32);
 }
 
-//１バイトのメモリ番地を取るjmp命令のエミュレート（より広い範囲に対応できるように）
-void near_jump(Emulator* emu){
-  int32_t diff = get_code32(emu, 1);
-  emu->eip += (diff + 5);
+static void add_rm32_r32(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t r32 = get_r32(emu, &modrm);
+    uint32_t rm32 = get_rm32(emu, &modrm);
+    set_rm32(emu, &modrm, rm32 + r32);
 }
 
-//ModR/MのModとRMで指定される32bitのレジスタorメモリに32bitの即値を格納
-static void mov_rm32_imm32(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-  uint32_t value = get_code32(emu, 0);
-  emu->eip += 4;
-  set_rm32(emu, &modrm, value);
+static void mov_rm32_r32(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t r32 = get_r32(emu, &modrm);
+    set_rm32(emu, &modrm, r32);
 }
 
-//ModR/MのModとRMで指定される32bitのレジスタorメモリにREGで指定される32ビットのレジスタの値を格納
-static void mov_rm32_r32(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-  uint32_t r32 = get_r32(emu, &modrm);
-  set_rm32(emu, &modrm, r32);
+static void push_r32(Emulator* emu)
+{
+    uint8_t reg = get_code8(emu, 0) - 0x50;
+    push32(emu, get_register32(emu, reg));
+    emu->eip += 1;
 }
 
-//REGで指定される32ビットのレジスタの値にModR/MのModとRMで指定される32bitのレジスタの値を格納
-static void mov_r32_rm32(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-  uint32_t rm32 = get_rm32(emu, &modrm);
-  set_r32(emu, &modrm, rm32);
+static void pop_r32(Emulator* emu)
+{
+    uint8_t reg = get_code8(emu, 0) - 0x58;
+    set_register32(emu, reg, pop32(emu));
+    emu->eip += 1;
 }
 
-//ModR/MのModとRMで指定される32bitのレジスタorメモリにREGで指定された32bitのレジスタの値を加算する
-static void add_rm32_r32(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-  uint32_t r32 = get_r32(emu, &modrm);
-  uint32_t rm32 = get_rm32(emu, &modrm);
-  set_rm32(emu, &modrm, r32 + rm32);
+static void push_imm32(Emulator* emu)
+{
+    uint32_t value = get_code32(emu, 1);
+    push32(emu, value);
+    emu->eip += 5;
 }
 
-//ModR/MのModとRMで指定される32bitのレジスタorメモリの値をインクリメントする
-static void inc_rm32(Emulator* emu, ModRM* modrm){
-  uint32_t value = get_rm32(emu, modrm);
-  set_rm32(emu, modrm, value++);
+static void push_imm8(Emulator* emu)
+{
+    uint8_t value = get_code8(emu, 1);
+    push32(emu, value);
+    emu->eip += 2;
 }
 
-static void code_ff(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-
-  switch (modrm.opecode)
-  {
-  case 0:
-    inc_rm32(emu, &modrm);
-    break;
-
-  default:
-    printf("not implemented: FF / %d\n", modrm.opecode);
-    exit(1);
-  }
+static void add_rm32_imm8(Emulator* emu, ModRM* modrm)
+{
+    uint32_t rm32 = get_rm32(emu, modrm);
+    uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
+    emu->eip += 1;
+    set_rm32(emu, modrm, rm32 + imm8);
 }
 
-//push操作
-static void push_r32(Emulator* emu){
-  uint8_t reg = get_code8(emu, 0) - 0x50;
-  push32(emu, get_register32(emu, reg));
-  emu->eip += 1;
+static void cmp_rm32_imm8(Emulator* emu, ModRM* modrm)
+{
+    uint32_t rm32 = get_rm32(emu, modrm);
+    uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
+    emu->eip += 1;
+    uint64_t result = (uint64_t)rm32 - (uint64_t)imm8;
+    update_eflags_sub(emu, rm32, imm8, result);
 }
 
-//pop操作
-static void pop_r32(Emulator* emu){
-  uint8_t reg = get_code8(emu, 0) - 0x58;
-  set_register32(emu, reg, pop32(emu));
-  emu->eip += 1;
+static void sub_rm32_imm8(Emulator* emu, ModRM* modrm)
+{
+    uint32_t rm32 = get_rm32(emu, modrm);
+    uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
+    emu->eip += 1;
+    uint64_t result = (uint64_t)rm32 - (uint64_t)imm8;
+    set_rm32(emu, modrm, result);
+    update_eflags_sub(emu, rm32, imm8, result);
 }
 
-//call命令
-static void call_rel32(Emulator* emu){
-  int32_t diff = get_sign_code32(emu, 1);
-  push32(emu, emu->eip + 5);
-  emu->eip += (diff + 5);
+static void code_83(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+
+    switch (modrm.opecode) {
+    case 0:
+        add_rm32_imm8(emu, &modrm);
+        break;
+    case 5:
+        sub_rm32_imm8(emu, &modrm);
+        break;
+    case 7:
+        cmp_rm32_imm8(emu, &modrm);
+        break;
+    default:
+        printf("not implemented: 83 /%d\n", modrm.opecode);
+        exit(1);
+    }
 }
 
-//ret命令
-static void ret(Emulator* emu){
-  emu->eip = pop32(emu);
+static void mov_rm32_imm32(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t value = get_code32(emu, 0);
+    emu->eip += 4;
+    set_rm32(emu, &modrm, value);
 }
 
-//leave命令の実行
-static void leave(Emulator* emu){
-  uint32_t ebp = get_register32(emu, EBP);
-  set_register32(emu, ESP, ebp);
-  set_register32(emu, EBP, pop32(emu));
-  emu->eip += 1;
+static void inc_rm32(Emulator* emu, ModRM* modrm)
+{
+    uint32_t value = get_rm32(emu, modrm);
+    set_rm32(emu, modrm, value + 1);
 }
 
-//push imm8の実装
-static void push_imm32(Emulator* emu){
-  uint32_t value = get_code32(emu, 1);
-  push32(emu, value);
-  emu->eip += 5;
+static void code_ff(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+
+    switch (modrm.opecode) {
+    case 0:
+        inc_rm32(emu, &modrm);
+        break;
+    default:
+        printf("not implemented: FF /%d\n", modrm.opecode);
+        exit(1);
+    }
 }
 
-static void push_imm8(Emulator* emu){
-  uint8_t value = get_code8(emu, 1);
-  push32(emu, value);
-  emu->eip += 2;
+static void call_rel32(Emulator* emu)
+{
+    int32_t diff = get_sign_code32(emu, 1);
+    push32(emu, emu->eip + 5);
+    emu->eip += (diff + 5);
 }
 
-static void add_rm32_imm8(Emulator* emu, ModRM* modrm){
-  uint32_t rm32 = get_rm32(emu, modrm);
-  uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
-  emu->eip += 1;
-  set_rm32(emu, modrm, rm32 + imm8);
+static void ret(Emulator* emu)
+{
+    emu->eip = pop32(emu);
 }
 
+static void leave(Emulator* emu)
+{
+    uint32_t ebp = get_register32(emu, EBP);
+    set_register32(emu, ESP, ebp);
+    set_register32(emu, EBP, pop32(emu));
 
-static void cmp_r32_rm32(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-  uint32_t r32 = get_r32(emu, &modrm);
-  uint32_t rm32 = get_rm32(emu, &modrm);
-  uint64_t result = (uint64_t)r32 - (uint64_t)rm32;
-  update_eflags_sub(emu, r32, rm32, result);
+    emu->eip += 1;
 }
 
-static void cmp_rm32_imm8(Emulator* emu, ModRM* modrm){
-  uint32_t rm32 = get_rm32(emu, modrm);
-  uint32_t imm8 = (uint32_t)get_sign_code8(emu, 0);
-  emu->eip += 1;
-  uint64_t result = (uint64_t)rm32 - (uint64_t)imm8;
-  update_eflags_sub(emu, rm32, imm8, result);
+static void short_jump(Emulator* emu)
+{
+    int8_t diff = get_sign_code8(emu, 1);
+    emu->eip += (diff + 2);
 }
 
-//ModR/MのModとRMで指定される32bitのレジスタorメモリに8bitの即値を減算する
-static void sub_rm32_imm8(Emulator* emu, ModRM* modrm){
-  uint32_t rm32 = get_rm32(emu, modrm);
-  uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
-  emu->eip += 1;
-  uint64_t result = (uint64_t)rm32 - (uint64_t)imm8;
-  set_rm32(emu, modrm, result);
-  update_eflags_sub(emu, rm32, imm8, result);
+static void near_jump(Emulator* emu)
+{
+    int32_t diff = get_sign_code32(emu, 1);
+    emu->eip += (diff + 5);
 }
 
-static void code_83(Emulator* emu){
-  emu->eip += 1;
-  ModRM modrm;
-  parse_modrm(emu, &modrm);
-
-  switch (modrm.opecode)
-  {
-  case 0:
-    add_rm32_imm8(emu, &modrm);
-    break;
-  case 5:
-    sub_rm32_imm8(emu, &modrm);
-    break;
-  case 7:
-    sub_rm32_imm8(emu, &modrm);
-    break;
-
-  default:
-    printf("not implemtend: 83 / %d\n");
-    exit(1);
-  }
+static void cmp_r32_rm32(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t r32 = get_r32(emu, &modrm);
+    uint32_t rm32 = get_rm32(emu, &modrm);
+    uint64_t result = (uint64_t)r32 - (uint64_t)rm32;
+    update_eflags_sub(emu, r32, rm32, result);
 }
 
 #define DEFINE_JX(flag, is_flag) \
 static void j ## flag(Emulator* emu) \
 { \
-  int diff = is_flag(emu) ? get_sign_code8(emu, 1) : 0; \
-  emu->eip += (diff + 2); \
+    int diff = is_flag(emu) ? get_sign_code8(emu, 1) : 0; \
+    emu->eip += (diff + 2); \
 } \
 static void jn ## flag(Emulator* emu) \
 { \
-  int diff = is_flag(emu) ? 0 : get_sign_code8(emu, 1); \
-  emu->eip += (diff + 2); \
+    int diff = is_flag(emu) ? 0 : get_sign_code8(emu, 1); \
+    emu->eip += (diff + 2); \
 }
 
 DEFINE_JX(c, is_carry)
@@ -210,64 +221,62 @@ DEFINE_JX(o, is_overflow)
 
 #undef DEFINE_JX
 
-static void jl(Emulator* emu){
-  int diff = (is_sign(emu) != is_overflow(emu)) ? get_sign_code8(emu, 1) : 0;
-  emu->eip += (diff + 2);
+static void jl(Emulator* emu)
+{
+    int diff = (is_sign(emu) != is_overflow(emu)) ? get_sign_code8(emu, 1) : 0;
+    emu->eip += (diff + 2);
 }
 
-static void jle(Emulator* emu){
-  int diff = (is_zero(emu) || (is_sign(emu) != is_overflow(emu))) ? get_sign_code8(emu, 1) : 0;
-  emu->eip += (diff + 2);
+static void jle(Emulator* emu)
+{
+    int diff = (is_zero(emu) || (is_sign(emu) != is_overflow(emu))) ? get_sign_code8(emu, 1) : 0;
+    emu->eip += (diff + 2);
 }
 
-//関数ポインタ型の配列instructionsの作成と初期化
-//typedefを用いることにより関数ポインタ配列をわかりやすく記述している
-void init_instrutions(void){
-  int i;
-  memset(instructions, 0, sizeof(instructions));
-  instructions[0x01] = add_rm32_r32;
-  instructions[0x3B] = cmp_r32_rm32;
+void init_instructions(void)
+{
+    int i;
+    memset(instructions, 0, sizeof(instructions));
+    instructions[0x01] = add_rm32_r32;
 
-  for (i = 0; i < 8; i++)
-  {
-    instructions[0x50 + i] = push_r32;
-  }
+    instructions[0x3B] = cmp_r32_rm32;
 
-  for (i = 0; i < 0; i++)
-  {
-    instructions[0x58 + i] = pop_r32;
-  }
+    for (i = 0; i < 8; i++) {
+        instructions[0x50 + i] = push_r32;
+    }
 
-  instructions[0x68] = push_imm32;
-  instructions[0x6A] = push_imm8;
+    for (i = 0; i < 8; i++) {
+        instructions[0x58 + i] = pop_r32;
+    }
 
-  instructions[0x70] = jo;
-  instructions[0x71] = jno;
-  instructions[0x72] = jc;
-  instructions[0x73] = jnc;
-  instructions[0x74] = jz;
-  instructions[0x75] = jnz;
-  instructions[0x78] = js;
-  instructions[0x79] = jns;
-  instructions[0x7C] = jl;
-  instructions[0x7E] = jle;
+    instructions[0x68] = push_imm32;
+    instructions[0x6A] = push_imm8;
 
-  instructions[0x83] = code_83;
-  instructions[0x89] = mov_rm32_r32;
-  instructions[0x8B] = mov_r32_rm32;
-  for (int i = 0; i < 8; i++)
-  {
-    instructions[0xB8 + i] = mov_r32_imm32;
-  }
+    instructions[0x70] = jo;
+    instructions[0x71] = jno;
+    instructions[0x72] = jc;
+    instructions[0x73] = jnc;
+    instructions[0x74] = jz;
+    instructions[0x75] = jnz;
+    instructions[0x78] = js;
+    instructions[0x79] = jns;
+    instructions[0x7C] = jl;
+    instructions[0x7E] = jle;
 
-  instructions[0xC3] = ret;
-  instructions[0xC7] = mov_rm32_imm32;
-  instructions[0xC9] = leave;
+    instructions[0x83] = code_83;
+    instructions[0x89] = mov_rm32_r32;
+    instructions[0x8B] = mov_r32_rm32;
 
-  instructions[0xE8] = call_rel32;
-  instructions[0xE9] = near_jump;
-  instructions[0xEB] = short_jump;
-  instructions[0xFF] = code_ff;
+    for (i = 0; i < 8; i++) {
+        instructions[0xB8 + i] = mov_r32_imm32;
+    }
+
+    instructions[0xC3] = ret;
+    instructions[0xC7] = mov_rm32_imm32;
+    instructions[0xC9] = leave;
+
+    instructions[0xE8] = call_rel32;
+    instructions[0xE9] = near_jump;
+    instructions[0xEB] = short_jump;
+    instructions[0xFF] = code_ff;
 }
-
-
