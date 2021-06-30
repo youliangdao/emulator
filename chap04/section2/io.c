d2465114543ca46ea2a944afb949901ad54ada90
youliangdao <youliangdaojing5@gmail.com>
2021-06-30 13:01:54 +0900

    test

---
+++ chap04/section2/io.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "instruction.h"
+#include "emulator.h"
+#include "emulator_function.h"
+
+#include "modrm.h"
+#include "io.h"
+
+uint8_t in_io8(uint16_t address)
+{
+  switch (address)
+  {
+  case 0x3f8:
+    return getchar();
+
+  default:
+    return 0;
+  }
+}
+
+void out_io8(uint16_t address, uint8_t value)
+{
+  switch (address)
+  {
+  case 0x3f8:
+    putchar(value);
+    break;
+  }
+}
