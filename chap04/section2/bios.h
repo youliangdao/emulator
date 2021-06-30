498284f399a204e5ac48345196780a2abe3bd84e
youliangdao <youliangdaojing5@gmail.com>
2021-06-30 14:15:09 +0900

    fix

--- chap04/section2/bios.h
+++ chap04/section2/bios.h
@@ -1,9 +1,7 @@
 #ifndef __BIOS_H__
 #define __BIOS_H__
 #include "emulator.h"
-#include <stdio.h>
 
 void bios_video(Emulator* emu);
-static void bios_video_teletype(Emulator* emu);
-static void put_string(const char* s, size_t n);
+
 #endif
